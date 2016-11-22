eac7256c973eb8258aa9bb2b97061e7828bc86f6
Unknown <brockljakub@gmail.com>
2016-11-22 07:12:59 +0100

    Created show method in Page controller
    also added show.html.erb in pages view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,18 @@
 class PagesController < ApplicationController
+  def show
+    @page = Page.find(params[:id])
+  end
   def new
 
   end
   def create
-    @page = Page.new(params[:page])
+    @page = Page.new(page_params)
     @page.save
     redirect_to @page
   end
+end
+
+private
+def page_params
+  params.require(:page).permit(:title, :test)
 end
