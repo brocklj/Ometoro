3cd321c6806f08eec97c4b38752172b31f87d40d
Unknown <brockljakub@gmail.com>
2016-11-27 17:09:54 +0100

    Dependecies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-  resources :pages, :topics, :courses
+  resources :pages do
+    resources :pages
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
