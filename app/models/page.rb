f204c8cd15f9950f4b5add14f49d7b7d47ab9cc0
Unknown <brockljakub@gmail.com>
2016-12-20 14:34:31 +0100

    Validated assiciation and seted default value

--- app/models/page.rb
+++ app/models/page.rb
@@ -7,11 +7,21 @@
 #  text       :text
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  visible    :boolean
 #
 
 class Page < ActiveRecord::Base
   has_many :courses
+  before_save :default_values
   validates :title, presence: true,
                     length: {minimum: 5}
   validates :visible, presence: false
+  before_save :default_values
+  validates_associated :courses
+  validates :visible, inclusion: { in: ["0", "1"] }
+
+
+  def default_values
+    self.text ||= "Default Text stranky"
+  end
 end
